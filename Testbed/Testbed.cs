60e5ee7070e7eecf3e346ec5d3d6b35675043984
AntoninPrazsky <33231531+AntoninPrazsky@users.noreply.github.com>
2019-12-31 10:04:09 +0100

    Pročištění

--- Testbed/Testbed.cs
+++ Testbed/Testbed.cs
@@ -12,8 +12,6 @@ using System;
 using System.Collections.Generic;
 using s = System.Numerics;
 
-//Tento projekt sloužil jako playground pro testování kódu, dál už nebude potřeba (30. 12. 2019)
-
 namespace Testbed
 {
 	public class Testbed : Game
@@ -193,25 +191,19 @@ namespace Testbed
 			map.PutBallAt(3, 2, 4, eBallType.Type1);
 			map.PutBallAt(3, 3, 4, eBallType.Type1);
 
-			//map.PutBallAt(4, 4, 9, eBallType.Type1);
 			map.PutBallAt(4, 5, 9, eBallType.Type2);
 			map.PutBallAt(4, 6, 9, eBallType.Type3);
-			//map.PutBallAt(4, 7, 9, eBallType.Type1);
 
 			map.PutBallAt(5, 4, 9, eBallType.Type2);
-			//map.PutBallAt(5, 5, 9, eBallType.Type3);
 			map.PutBallAt(5, 6, 9, eBallType.Type1);
 			map.PutBallAt(5, 7, 9, eBallType.Type1);
 
 			map.PutBallAt(6, 4, 9, eBallType.Type2);
 			map.PutBallAt(6, 5, 9, eBallType.Type3);
-			//map.PutBallAt(6, 6, 9, eBallType.Type1);
 			map.PutBallAt(6, 7, 9, eBallType.Type1);
 
-			//map.PutBallAt(7, 4, 9, eBallType.Type2);
 			map.PutBallAt(7, 5, 9, eBallType.Type3);
 			map.PutBallAt(7, 6, 9, eBallType.Type1);
-			//map.PutBallAt(7, 7, 9, eBallType.Type1);
 
 			map.PutBallAt(4, 4, 5, eBallType.Type1);
 			map.PutBallAt(4, 5, 5, eBallType.Type2);
@@ -228,10 +220,10 @@ namespace Testbed
 			map.PutBallAt(6, 6, 5, eBallType.Type1);
 			map.PutBallAt(6, 7, 5, eBallType.Type1);
 
-			map.PutBallAt(7, 4, 5, eBallType.Type2);
-			map.PutBallAt(7, 5, 5, eBallType.Type3);
+			map.PutBallAt(7, 4, 5, eBallType.Type1);
+			map.PutBallAt(7, 5, 5, eBallType.Type1);
 			map.PutBallAt(7, 6, 5, eBallType.Type1);
-			map.PutBallAt(7, 7, 5, eBallType.Type3);
+			map.PutBallAt(7, 7, 5, eBallType.Type1);
 
 			_balls.Add(BallsConstraintsBuilder.BuildBallsStructure(map.GetStaticBallsArray(), ref _simulation));
 
