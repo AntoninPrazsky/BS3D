ea5beada080b42f5cc5fb1d06abca339886eb53e
AntoninPrazsky <33231531+AntoninPrazsky@users.noreply.github.com>
2020-01-03 11:29:59 +0100

    Binární serializace a deserializace mapy kuliček pro BallsMap

--- BS3DLibs/Prazsky.BS3D/GameStructure/BallsMap.cs
+++ BS3DLibs/Prazsky.BS3D/GameStructure/BallsMap.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Prazsky.BS3D.GameStructure.DataBags;
 using Prazsky.Core.Camera;
 using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Prazsky.BS3D.GameStructure
 {
@@ -33,7 +36,6 @@ namespace Prazsky.BS3D.GameStructure
 		/// <param name="stageZ">Souřadnice Z v dané úrovni.</param>
 		/// <param name="level">Úroveň.</param>
 		/// <param name="type">Typ.</param>
-		/// <returns></returns>
 		public void PutBallAt(byte stageX, byte stageZ, byte level, eBallType type)
 		{
 			if (stageX > _stageSizeX || stageZ > _stageSizeZ || level > _levels) throw new ArgumentOutOfRangeException($"Invalid requested ball position, array size is: {_stageSizeX} × {_stageSizeZ} × {_levels}");
@@ -59,6 +61,18 @@ namespace Prazsky.BS3D.GameStructure
 			return _balls;
 		}
 
+		public int GetBallsCount()
+		{
+			int count = 0;
+
+			for (int level = 0; level < _levels; level++)
+				for (int x = 0; x < _stageSizeX; x++)
+					for (int z = 0; z < _stageSizeZ; z++)
+						if (_balls[x, z, level] != null)
+							count++;
+			return count;
+		}
+
 		public void Draw(ICamera camera)
 		{
 			for (int level = 0; level < _levels; level++)
@@ -66,6 +80,76 @@ namespace Prazsky.BS3D.GameStructure
 					for (int z = 0; z < _stageSizeZ; z++)
 						if (_balls[x, z, level] != null)
 							_balls[x, z, level].Draw(camera, _ballModel);
+		}
+
+		public void SerializeAsXML()
+		{
+			//Proč ne jako XML:
+			// 1) XmlSerializer neumí serializovat multidimenziální pole ([,,]), musel jsem ho ručně předělat na pole polí polí ([][][]).
+			// 2) Výsledné XML je zbytečně velké, protože místa, kde nejsou v mapě kuličky, jsou null - do XML se renderovaly prázdné elementy s atributem xsi:nil="true".
+			// 3) Trvá to dlouho (cca 6× déle než binární serializace a deserializace).
+
+			//Proč ano jako XML:
+			// 1) Výsledek může číst a editovat člověk.
+
+			throw new NotImplementedException();
+		}
+
+		public void DeserializeXML()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void SerializeAsBinary(string fileName)
+		{
+			//Proč ano jako Binary:
+			// 1) Výsledek je výrazně menší než XML.
+			// 2) Serializace i deserializace je výrazně rychlejší než serializace do XML (cca 6×).
+
+			//Proč ne jako Binary:
+			// 1) Výsledek nemůže číst ani editovat člověk.
+
+			BallPositionTypes ballPositionTypes = BuildBallPositionTypes();
+
+			BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+			using (Stream stream = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.Write))
+			{
+				binaryFormatter.Serialize(stream, ballPositionTypes);
+			}
+		}
+
+		public void DeserializeBinary(string fileName)
+		{
+			BallPositionTypes ballPositionTypes;
+
+			BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+			using (Stream stream = new FileStream(fileName, FileMode.Open))
+			{
+				ballPositionTypes = (BallPositionTypes)binaryFormatter.Deserialize(stream);
+			}
+		}
+
+		private BallPositionTypes BuildBallPositionTypes()
+		{
+			BallPositionTypes ballPositionTypes = new BallPositionTypes();
+
+			ballPositionTypes.Balls = new BallPositionType[_stageSizeX, _stageSizeZ, _levels];
+
+			for (int level = 0; level < _levels; level++)
+				for (int x = 0; x < _stageSizeX; x++)
+					for (int z = 0; z < _stageSizeZ; z++)
+						if (_balls[x, z, level] != null)
+							ballPositionTypes.Balls[x, z, level] = new BallPositionType()
+							{
+								PositionX = _balls[x, z, level].Position.X,
+								PositionY = _balls[x, z, level].Position.Y,
+								PositionZ = _balls[x, z, level].Position.Z,
+								Type = _balls[x, z, level].Type
+							};
+
+			return ballPositionTypes;
 		}
 	}
 }
---
+++ BS3DLibs/Prazsky.BS3D/GameStructure/DataBags/BallPositionType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Prazsky.BS3D.GameStructure.DataBags
+{
+	[Serializable]
+	public class BallPositionType
+	{
+		[DataMember]
+		public float PositionX;
+		[DataMember]
+		public float PositionY;
+		[DataMember]
+		public float PositionZ;
+		[DataMember]
+		public eBallType Type;
+	}
+
+	[Serializable]
+	public class BallPositionTypes
+	{
+		[DataMember]
+		public BallPositionType[,,] Balls;
+	}
+}
