7485a644e047fb9eeffbd8ee7ec5c4d558a7c0fa
Antonín Pražský <antonin.prazsky@hotmail.com>
2023-01-12 16:03:08 +0100

    Enable MSAA for Editor

--- MapEditor/MapEditor.cs
+++ MapEditor/MapEditor.cs
@@ -9,7 +9,7 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Windows.Forms;
-using mg = Microsoft.Xna.Framework.Input.Keys;
+using mgKeys = Microsoft.Xna.Framework.Input.Keys;
 
 namespace MapEditor
 {
@@ -33,8 +33,6 @@ namespace MapEditor
 
         private GraphicsDeviceManager _graphics;
         private bool _windowed;
-        private bool _preferHiDef;
-        private bool _preferMultiSampling;
 
         public Info Info { private set; get; }
 
@@ -42,14 +40,10 @@ namespace MapEditor
 
         public MapEditor(
             bool windowed = true,
-            bool preferHiDef = true,
-            bool preferMultiSampling = true,
             int windowWidth = 1920,
             int windowHeight = 1080)
         {
             _windowed = windowed;
-            _preferHiDef = preferHiDef;
-            _preferMultiSampling = preferMultiSampling;
 
             _graphics = new GraphicsDeviceManager(this);
 
@@ -91,19 +85,19 @@ namespace MapEditor
 
         private void SelectorControl()
         {
-            if (_cih.PressedOnce(mg.Up, Buttons.DPadUp)) _selector.Move(Vector3.Forward);
-            if (_cih.PressedOnce(mg.Down, Buttons.DPadDown)) _selector.Move(Vector3.Backward);
-            if (_cih.PressedOnce(mg.Left, Buttons.DPadLeft)) _selector.Move(Vector3.Left);
-            if (_cih.PressedOnce(mg.Right, Buttons.DPadRight)) _selector.Move(Vector3.Right);
-            if (_cih.PressedOnce(mg.PageUp, Buttons.RightShoulder)) _selector.Move(Vector3.Up);
-            if (_cih.PressedOnce(mg.PageDown, Buttons.LeftShoulder)) _selector.Move(Vector3.Down);
+            if (_cih.PressedOnce(mgKeys.Up, Buttons.DPadUp)) _selector.Move(Vector3.Forward);
+            if (_cih.PressedOnce(mgKeys.Down, Buttons.DPadDown)) _selector.Move(Vector3.Backward);
+            if (_cih.PressedOnce(mgKeys.Left, Buttons.DPadLeft)) _selector.Move(Vector3.Left);
+            if (_cih.PressedOnce(mgKeys.Right, Buttons.DPadRight)) _selector.Move(Vector3.Right);
+            if (_cih.PressedOnce(mgKeys.PageUp, Buttons.RightShoulder)) _selector.Move(Vector3.Up);
+            if (_cih.PressedOnce(mgKeys.PageDown, Buttons.LeftShoulder)) _selector.Move(Vector3.Down);
 
-            if (_cih.PressedOnce(mg.Space, Buttons.A)) _selector.PutBall();
-            if (_cih.PressedOnce(mg.Delete, Buttons.B)) _selector.RemoveBall();
+            if (_cih.PressedOnce(mgKeys.Space, Buttons.A)) _selector.PutBall();
+            if (_cih.PressedOnce(mgKeys.Delete, Buttons.B)) _selector.RemoveBall();
 
-            if (_cih.PressedOnce(mg.NumPad1)) _selector.ChangeBallType(eBallType.Type1);
-            if (_cih.PressedOnce(mg.NumPad2)) _selector.ChangeBallType(eBallType.Type2);
-            if (_cih.PressedOnce(mg.NumPad3)) _selector.ChangeBallType(eBallType.Type3);
+            if (_cih.PressedOnce(mgKeys.NumPad1)) _selector.ChangeBallType(eBallType.Type1);
+            if (_cih.PressedOnce(mgKeys.NumPad2)) _selector.ChangeBallType(eBallType.Type2);
+            if (_cih.PressedOnce(mgKeys.NumPad3)) _selector.ChangeBallType(eBallType.Type3);
         }
 
         protected override void Update(GameTime gameTime)
@@ -112,27 +106,27 @@ namespace MapEditor
             {
                 _cih.RegisterCurrentInputState();
 
-                if (_cih.PressedOnce(mg.Escape, Buttons.Back)) Exit();
+                if (_cih.PressedOnce(mgKeys.Escape, Buttons.Back)) Exit();
 
                 SelectorControl();
 
                 _cih.CameraMovement(gameTime);
 
-                if (_cih.PressedOnce(mg.F12, Buttons.Start)) Info.Visible = !Info.Visible;
-                if (_cih.PressedOnce(mg.M, Buttons.X)) _draw = !_draw;
+                if (_cih.PressedOnce(mgKeys.F12, Buttons.Start)) Info.Visible = !Info.Visible;
+                if (_cih.PressedOnce(mgKeys.M, Buttons.X)) _draw = !_draw;
 
-                if (_cih.PressedOnce(mg.N)) FullMapTest();
+                if (_cih.PressedOnce(mgKeys.N)) FullMapTest();
 
-                if (_cih.PressedOnce(mg.D1)) _cih.CenterCameraToMapCenter(_map.GetStaticBallsMapCenter(), Vector3.Forward);
-                if (_cih.PressedOnce(mg.D2)) _cih.CenterCameraToMapCenter(_map.GetStaticBallsMapCenter(), Vector3.Backward);
-                if (_cih.PressedOnce(mg.D3)) _cih.CenterCameraToMapCenter(_map.GetStaticBallsMapCenter(), Vector3.Left);
-                if (_cih.PressedOnce(mg.D4)) _cih.CenterCameraToMapCenter(_map.GetStaticBallsMapCenter(), Vector3.Right);
-                if (_cih.PressedOnce(mg.D5)) _cih.CenterCameraToMapCenter(_map.GetStaticBallsMapCenter(), Vector3.Up);
-                if (_cih.PressedOnce(mg.D6)) _cih.CenterCameraToMapCenter(_map.GetStaticBallsMapCenter(), Vector3.Down);
+                if (_cih.PressedOnce(mgKeys.D1)) _cih.CenterCameraToMapCenter(_map.GetStaticBallsMapCenter(), Vector3.Forward);
+                if (_cih.PressedOnce(mgKeys.D2)) _cih.CenterCameraToMapCenter(_map.GetStaticBallsMapCenter(), Vector3.Backward);
+                if (_cih.PressedOnce(mgKeys.D3)) _cih.CenterCameraToMapCenter(_map.GetStaticBallsMapCenter(), Vector3.Left);
+                if (_cih.PressedOnce(mgKeys.D4)) _cih.CenterCameraToMapCenter(_map.GetStaticBallsMapCenter(), Vector3.Right);
+                if (_cih.PressedOnce(mgKeys.D5)) _cih.CenterCameraToMapCenter(_map.GetStaticBallsMapCenter(), Vector3.Up);
+                if (_cih.PressedOnce(mgKeys.D6)) _cih.CenterCameraToMapCenter(_map.GetStaticBallsMapCenter(), Vector3.Down);
 
-                if (_cih.PressedOnce(mg.R)) _cih.RestartCamera();
+                if (_cih.PressedOnce(mgKeys.R)) _cih.RestartCamera();
 
-                if (_cih.PressedOnce(mg.F1))
+                if (_cih.PressedOnce(mgKeys.F1))
                 {
                     string filePath = GetFilePathByDialog(true);
                     if (!string.IsNullOrEmpty(filePath))
@@ -145,7 +139,7 @@ namespace MapEditor
                     }
                 }
 
-                if (_cih.PressedOnce(mg.F2))
+                if (_cih.PressedOnce(mgKeys.F2))
                 {
                     string filePath = GetFilePathByDialog(false);
                     if (!string.IsNullOrEmpty(filePath))
@@ -230,8 +224,7 @@ namespace MapEditor
             _graphics.IsFullScreen = !windowed;
 
             _graphics.SynchronizeWithVerticalRetrace = true;
-
-            _graphics.PreferMultiSampling = _preferMultiSampling;
+            _graphics.PreferMultiSampling = true;
 
             _graphics.ApplyChanges();
 
@@ -241,12 +234,9 @@ namespace MapEditor
 
         private void _graphics_PreparingDeviceSettings(object sender, PreparingDeviceSettingsEventArgs e)
         {
-            e.GraphicsDeviceInformation.PresentationParameters.PresentationInterval = PresentInterval.Default; //Default
-
-            if (_preferHiDef && e.GraphicsDeviceInformation.Adapter.IsProfileSupported(GraphicsProfile.HiDef))
-                e.GraphicsDeviceInformation.GraphicsProfile = GraphicsProfile.HiDef;
-            else
-                e.GraphicsDeviceInformation.GraphicsProfile = GraphicsProfile.Reach;
+            e.GraphicsDeviceInformation.PresentationParameters.PresentationInterval = PresentInterval.Default;
+            e.GraphicsDeviceInformation.GraphicsProfile = GraphicsProfile.HiDef;
+            e.GraphicsDeviceInformation.PresentationParameters.MultiSampleCount = 16;
         }
 
         protected override void UnloadContent()
@@ -268,20 +258,19 @@ namespace MapEditor
 
             Array ballTypes = Enum.GetValues(typeof(eBallType));
             Random random = new Random();
+			eBallType lastBallType = eBallType.Type1;
 
-            eBallType currentBallType = eBallType.Type1;
-            eBallType lastBallType = eBallType.Type1;
-
-            for (byte x = 0; x < sizeX; x++)
+			for (byte x = 0; x < sizeX; x++)
                 for (byte z = 0; z < sizeZ; z++)
                     for (byte l = 0; l < levels; l++)
                     {
-                        do
-                        {
-                            currentBallType = (eBallType)ballTypes.GetValue(random.Next(ballTypes.Length));
-                        } while (currentBallType == lastBallType);
+						eBallType currentBallType;
+						do
+						{
+							currentBallType = (eBallType)ballTypes.GetValue(random.Next(ballTypes.Length));
+						} while (currentBallType == lastBallType);
 
-                        _map.PutBallAt(x, z, l, currentBallType);
+						_map.PutBallAt(x, z, l, currentBallType);
 
                         lastBallType = currentBallType;
                     }
